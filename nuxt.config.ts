5f9d8859388d6bcbc2eacff27a0f3939929d0dfa
Miracle Onyenma <42788603+miracleonyenma@users.noreply.github.com>
2024-01-23 22:09:20 +0000

    init: initial commit
    - Set up Nuxt, Nuxt UI, Shadcn/vue
    - Add new files and configurations

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: [ 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
