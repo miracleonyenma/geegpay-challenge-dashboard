de4eee5fb82d46e8a1471104d9f0e5d9a08fc3ee
miracleonyenma <miracleficient@gmail.com>
2024-01-26 16:20:27 +0100

    updates: Add SEO meta tags and cover images

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+const siteURL = ref("https://geeg-challenge-dash.netlify.app/");
+useSeoMeta({
+  title: "Sales Dashboard | Geegpay",
+  description: "A sales dashboard for the Geegpay frontend challenge",
+  ogDescription: "A sales dashboard for the Geegpay frontend challenge",
+  ogTitle: "Sales Dashboard | Geegpay",
+  ogUrl: siteURL.value,
+  ogImage: `${siteURL.value}/cover.jpg`,
+  ogSiteName: "Sales Dashboard",
+  ogType: "website",
+  ogLocale: "en_US",
+  twitterCard: "summary_large_image",
+  twitterSite: "@miracleio",
+  twitterTitle: "Sales Dashboard | Geegpay",
+  twitterDescription: "A sales dashboard for the Geegpay frontend challenge",
+  twitterImage: `${siteURL.value}/cover.jpg`,
+});
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
